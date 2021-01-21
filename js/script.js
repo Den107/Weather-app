96aa621c5989ca93d6ef8f1a589aa7094de598fc
Den107 <deniskrv@gmail.com>
2021-01-21 17:12:16 +0300

    first step complete

--- js/script.js
+++ js/script.js
@@ -3,26 +3,45 @@ const weatherBlock = document.querySelector('.weather-block'),
     cityName = weatherBlock.querySelector('.weather-block__city-name'),
     cityTemp = weatherBlock.querySelector('.weather-block__temp'),
     cityWeather = weatherBlock.querySelector('.weather-block__cloudy'),
-    icon = weatherBlock.querySelector('.weather-block__icon');
+    icon = weatherBlock.querySelector('.weather-block__icon'),
+    searchForm = document.querySelector('.search-form'),
+    searchCity = searchForm.querySelector('.search-form__input'),
+    searchBtn = searchForm.querySelector('.search-form__button');
+
 function clear() {
     cityName.textContent = '';
     cityTemp.textContent = '';
     cityWeather.textContent = '';
     icon.textContent = '';
 }
+
 clear();
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=london&appid=${API_KEY}`)
-    .then(function (resp) { return resp.json(); })
-    .then(function (data) {
-        console.log(data);
-        cityName.textContent = data.name;
-        cityTemp.innerHTML = `${Math.floor(data.main.temp - 273.15)}&degC`;
-        cityWeather.textContent = data.weather[0].main;
-        icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="icon">`;
-    })
-    .catch(function () {
-        console.log(new Error('error'));
-    });
+getWheather();
+function getWheather(city = 'London') {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+        .then(function (resp) { return resp.json(); })
+        .then(function (data) {
+            console.log(data);
+            cityName.textContent = data.name;
+            cityTemp.innerHTML = `${Math.floor(data.main.temp - 273.15)}&degC`;
+            cityWeather.textContent = data.weather[0].main;
+            icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="icon">`;
+        })
+        .catch(function () {
+            console.log(new Error('error'));
+        });
+}
+
+
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let city = searchCity.value.toLowerCase();
+    if (!city || typeof city !== 'string') {
+        searchCity.attributes.placeholder.textContent = 'Insert city name';
+    } else {
+        getWheather(city);
+    }
+});
 
 
 
